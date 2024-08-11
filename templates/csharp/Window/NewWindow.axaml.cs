1863ce27badb0f3961292c8d46332809d40c9433
Damian Suess <djsuess@gmail.com>
2024-08-11 08:46:28 -0400

    single file templates

---
+++ templates/csharp/Window/NewWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PrismAvaloniaTemplate.Namespace;
+
+public partial class NewWindow : Window
+{
+    public NewWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ templates/csharp/event/NewEvent.cs
@@ -0,0 +1,8 @@
+using Prism.Events;
+
+namespace PrismAvaloniaTemplate.Events;
+
+public class NewEvent : PubSubEvent
+{
+    
+}
